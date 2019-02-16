c12ef8bc7d6a2f33588bcb6c180572dbe7a6d4a0
edcesar <edcesar@edcesar.com>
2019-02-16 08:31:03 -0200

    Add test assertTrue

--- Test.php
+++ Test.php
@@ -2,6 +2,11 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
+	public function testOk()
+	{
+		$this->assertTrue(true);
+	}
+
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,1);
   	}
