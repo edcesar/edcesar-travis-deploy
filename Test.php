7978af5d5fb84c8e3c5dcd409215b20a7d43ef5b
edcesar <edcesar@edcesar.com>
2019-02-16 16:08:32 -0200

    teste

--- Test.php
+++ Test.php
@@ -5,11 +5,10 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOk()
 	{
 		$this->assertTrue(true);
+		$this->assertTrue(true);
 	}
 
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
 }
-
-?>
